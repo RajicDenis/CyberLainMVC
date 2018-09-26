a6a011665b463d2610214be9e5abf900775d209e
RajicDenis <denis.rajic92@gmail.com>
2018-09-26 13:10:28 +0200

    Welcome page

--- views/welcome.php
+++ views/welcome.php
@@ -1,18 +1,35 @@
-<h1>Welcome to CyberLainMVC!!</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>CyberLainMVC</title>
 
-<pre>
-<?php print_r($data); ?>
-</pre>
-<form action="/about" method="POST">
-	<input type="text" name="name" placeholder="Name">
-	<input type="text" name="email" placeholder="Email">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-	<button type="submit">Submit</button>
-</form>
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Merienda" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Kalam" rel="stylesheet">
 
+	<link rel="stylesheet" href="../public/css/main.css">
+</head>
+<body>
 
+	<div class="wrapper">
+			
+		<div class="title d-flex justify-content-center">
+			<div class="t-box"><span>CyberL</span><img class="logo" src="../public/images/leaf.png"><span class="lspace"> inMVC</span></div>
+			<hr class="style-two">
+		</div>
+		
+		<div class="links">
+			<a href="https://github.com/RajicDenis/CyberLainMVC">GITHUB</a>
+		</div>
 
+	</div>
 
+	
+</body>
+</html>
 
 
 
